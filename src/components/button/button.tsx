97d2183c75e9000152364f6e4179ca7f2ec81958
chendou <1260528932@qq.com>
2020-12-06 22:42:53 +0800

    'button'

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,30 +1,46 @@
-import React from 'react'
+import React from 'react';
 import classNames from 'classnames';
-
+import './button.scss'
 const tuple = <T extends string[]>(...args: T) => args;
-const ButtonTypes = tuple('default', 'primary', 'ghost', 'dashed', 'link', 'text');
-const ButtonSizes = tuple('large','middle','small')
+const ButtonTypes = tuple(
+  'default',
+  'primary',
+  'ghost',
+  'dashed',
+  'link',
+  'text'
+);
+const ButtonSizes = tuple('large', 'middle', 'small');
 type ButtonType = typeof ButtonTypes[number];
 type ButtonSize = typeof ButtonSizes[number];
 interface BaseButtonProps {
-  type?:ButtonType,
-  size?:ButtonSize
+  btnType?: ButtonType;
+  size?: ButtonSize;
+  disabled?: boolean;
+  className?: string;
+  children?: React.ReactNode;
 }
+type ButtonProps =  React.ButtonHTMLAttributes<HTMLElement> & BaseButtonProps
 
-const Button:React.FC<BaseButtonProps> = (props) => {
-  const {type,size} = props
-  console.log(type)
-  const classes = classNames(
-    'btn',
-    {
-      [`btn-${type}`]:type,
-      [`btn-${size}`]:size
-    }
-  )
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const { btnType, size, children, disabled } = props;
+  console.log(btnType);
+  const classes = classNames('btn', {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+  });
   return (
     <>
-      <button className={classes}>233</button>
+      <button disabled={disabled} className={classes}>
+        {children}
+      </button>
     </>
-  )
-}
-export default Button
+  );
+};
+Button.defaultProps = {
+  disabled: false,
+  btnType: 'default',
+  size: 'middle',
+};
+export default Button;
